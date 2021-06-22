a35fff77c002b62cf9c4017fc820676997623bec
Belov <yevgeniyb@checkpoint.com>
2021-06-22 16:42:55 +0300

    improve mocks

--- src/mockups/kubernetes/_1.vue
+++ src/mockups/kubernetes/_1.vue
@@ -1,135 +1,187 @@
 <template>
-	<div class="absolute inset-0 theme theme-light flex flex-col">
-		<div class="flex-0 border-b px-4 py-2 theme theme-dark-primary font-light uppercase">Title</div>
-		<div class="flex flex-1 flex-row">
-			<div class="flex-0 flex flex-col border-r theme theme-dark-primary p-6 text-xs">
-				<div class="flex items-center ">
-					<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full"
-						><svg
-							class="w-4 h-4"
-							xmlns="http://www.w3.org/2000/svg"
-							xmlns:xlink="http://www.w3.org/1999/xlink"
-							version="1.1"
-							x="0px"
-							y="0px"
-							viewBox="0 0 76.887 55.552"
-							style="enable-background: new 0 0 76.887 55.552"
-							xml:space="preserve"
-						>
-							<polygon
-								style="fill: var(--color-primary)"
-								points="34.373,49.541 76.887,5.889 70.739,0 28.426,43.458 6.078,20.632 0,26.585 22.488,49.557    22.488,49.557 28.517,55.552 28.426,55.467 28.517,55.552 34.373,49.541  "
-							/></svg
-					></div>
-					<div class="ml-2 uppercase">CONFIGURATIONS</div>
-				</div>
-				<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
-				<div class="flex items-center">
-					<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full"
-						><svg
-							class="w-4 h-4"
-							xmlns="http://www.w3.org/2000/svg"
-							xmlns:xlink="http://www.w3.org/1999/xlink"
-							version="1.1"
-							x="0px"
-							y="0px"
-							viewBox="0 0 76.887 55.552"
-							style="enable-background: new 0 0 76.887 55.552"
-							xml:space="preserve"
-						>
-							<polygon
-								style="fill: var(--color-primary)"
-								points="34.373,49.541 76.887,5.889 70.739,0 28.426,43.458 6.078,20.632 0,26.585 22.488,49.557    22.488,49.557 28.517,55.552 28.426,55.467 28.517,55.552 34.373,49.541  "
-							/></svg
-					></div>
-					<div class="ml-2 uppercase">UNIT</div>
-				</div>
-				<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
-				<div class="flex items-center">
-					<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full"
-						><svg
-							class="w-4 h-4"
-							xmlns="http://www.w3.org/2000/svg"
-							xmlns:xlink="http://www.w3.org/1999/xlink"
-							version="1.1"
-							x="0px"
-							y="0px"
-							viewBox="0 0 76.887 55.552"
-							style="enable-background: new 0 0 76.887 55.552"
-							xml:space="preserve"
-						>
-							<polygon
-								style="fill: var(--color-primary)"
-								points="34.373,49.541 76.887,5.889 70.739,0 28.426,43.458 6.078,20.632 0,26.585 22.488,49.557    22.488,49.557 28.517,55.552 28.426,55.467 28.517,55.552 34.373,49.541  "
-							/></svg
-					></div>
-					<div class="ml-2 uppercase">INSTRUCTIONS</div>
-				</div>
-				<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
-				<div class="flex items-center">
-					<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full">
-						<div class="text-md font-semibold" style="color: var(--color-primary)">3</div>
+	<div class="flex flex-1 items-center justify-center">
+		<div class="theme theme-light flex flex-col relative overflow-hidden shadow-xl shadow-strong">
+			<div class="flex-0 border-b px-4 py-2 theme theme-dark-primary font-light uppercase"
+				>KUBERNETES ONBOARDING</div
+			>
+			<div class="flex flex-1 flex-row">
+				<div class="flex-0 flex flex-col border-r theme theme-dark-primary p-6 text-xs">
+					<div class="flex items-center">
+						<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full"
+							><svg
+								class="w-4 h-4"
+								xmlns="http://www.w3.org/2000/svg"
+								xmlns:xlink="http://www.w3.org/1999/xlink"
+								version="1.1"
+								x="0px"
+								y="0px"
+								viewBox="0 0 76.887 55.552"
+								style="enable-background: new 0 0 76.887 55.552"
+								xml:space="preserve"
+							>
+								<polygon
+									style="fill: var(--color-primary)"
+									points="34.373,49.541 76.887,5.889 70.739,0 28.426,43.458 6.078,20.632 0,26.585 22.488,49.557    22.488,49.557 28.517,55.552 28.426,55.467 28.517,55.552 34.373,49.541  "
+								/></svg
+						></div>
+						<div class="ml-2 uppercase">CONFIGURATIONS</div>
 					</div>
-					<div class="ml-2 uppercase">Summary</div>
+					<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
+					<div class="flex items-center">
+						<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full"
+							><svg
+								class="w-4 h-4"
+								xmlns="http://www.w3.org/2000/svg"
+								xmlns:xlink="http://www.w3.org/1999/xlink"
+								version="1.1"
+								x="0px"
+								y="0px"
+								viewBox="0 0 76.887 55.552"
+								style="enable-background: new 0 0 76.887 55.552"
+								xml:space="preserve"
+							>
+								<polygon
+									style="fill: var(--color-primary)"
+									points="34.373,49.541 76.887,5.889 70.739,0 28.426,43.458 6.078,20.632 0,26.585 22.488,49.557    22.488,49.557 28.517,55.552 28.426,55.467 28.517,55.552 34.373,49.541  "
+								/></svg
+						></div>
+						<div class="ml-2 uppercase">UNIT</div>
+					</div>
+					<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
+					<div class="flex items-center">
+						<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full"
+							><svg
+								class="w-4 h-4"
+								xmlns="http://www.w3.org/2000/svg"
+								xmlns:xlink="http://www.w3.org/1999/xlink"
+								version="1.1"
+								x="0px"
+								y="0px"
+								viewBox="0 0 76.887 55.552"
+								style="enable-background: new 0 0 76.887 55.552"
+								xml:space="preserve"
+							>
+								<polygon
+									style="fill: var(--color-primary)"
+									points="34.373,49.541 76.887,5.889 70.739,0 28.426,43.458 6.078,20.632 0,26.585 22.488,49.557    22.488,49.557 28.517,55.552 28.426,55.467 28.517,55.552 34.373,49.541  "
+								/></svg
+						></div>
+						<div class="ml-2 uppercase">INSTRUCTIONS</div>
+					</div>
+					<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
+					<div class="flex items-center">
+						<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full">
+							<div class="text-md font-semibold" style="color: var(--color-primary)">4</div>
+						</div>
+						<div class="ml-2 uppercase">Summary</div>
+					</div>
 				</div>
-			</div>
-			<div class="flex flex-1 flex-col">
-				<div class="flex-1 p-6 bg-canvas"> 
+				<div class="flex flex-1 flex-col max-w-3xl">
+					<div class="flex-1 p-6 bg-canvas">
+						<div class="text-2xl">Onboarding Summary</div>
+						<div class="bg-info-lightest p-2 text-xs flex items-center mt-3">
+							<div class="flex items-center justify-center">
+								<svg width="16" height="16" viewBox="0 0 16 16">
+									<path
+										d="M8,0a8,8,0,1,0,8,8A8,8,0,0,0,8,0ZM8,13H8a1,1,0,0,1-1-1V7A1,1,0,0,1,8,6H8A1,1,0,0,1,9,7v5A1,1,0,0,1,8,13ZM9,4A1,1,0,1,1,8,3,1,1,0,0,1,9,4Z"
+									/>
+								</svg>
+							</div>
+							<div class="flex-1 ml-2">
+								To validate the onboarding and test the connectivity, install the agent on the cluster.
+							</div>
+						</div>
+						<div class="grid grid-cols-5 gap-6 my-8">
+							<div class="font-semibold">Claster:</div>
+							<div class="col-span-4">My_Cluster</div>
 
-<div class="text-2xl -font-light">Onboarding Summary</div>
-<div class="">Kubernetes cluster My_Cluster has been successfully created and is now waiting for the agent to initate communication.</div>
-<div class="">To validate the onboarding and test the connectivity, install the agent on the cluster and click on </div>
-<button class="border bg-content px-2 py-1">Validate</button>
+							<div class="font-semibold">Deployment:</div>
+							<div class="col-span-4">
+								<div class="">
+									<div class="flex">
+										<div class="bg-content border flex-1 self-center">
+											<div class="h-1 w-4/6 bg-info-darkest"></div>
+										</div>
+										<div class="flex justify-end ml-2"> 30 / 63 </div>
+									</div>
+								</div>
+							</div>
 
-<div class="bg-aside border my-8">
-	<div class="h-1 w-4/6 bg-info-darkest"></div>
-</div>
+							<div class="font-semibold">Status:</div>
 
-<div class="">
-	The agent has not initated communication.
-Once the agent is installed please click on the "VALIDATE" button again.
-If the agent has already been installed please check the agent connectivity. See <a href="text-info">sk165514</a>
-
-</div>
-
-				</div>
-				<div class="flex-0 border-t py-2 px-4 flex bg-aside">
-					<div class="ml-auto space-x-2">
-						<button
-							class="
-							bg-canvas
-							border
-							border-weaker
-							text-weakest
-								justify-center
-								hover:bg-content
-								hover:border
-								active:bg-content-dark
-								active:border-weakest
-								focus:outline-none
-								px-3
-								py-1
-								min-w-[10ch]
-							"
-							>Back</button
-						>
-						<button
-							class="
-								theme theme-dark
-								justify-center
-								bg-primary
-								hover:bg-hover
-								hover:border-stronger
-								active:bg-primary-dark
-								active:border-weakest
-								border
-								focus:outline-none
-								px-3
-								py-1
-								min-w-[10ch]
-							"
-							>FINISH</button
-						>
+							<div class="col-span-4">
+								<div class="flex flex-1">
+									<svg
+										class="w-4 h-4"
+										fill="var(--color-warning)"
+										id="Layer_1"
+										data-name="Layer 1"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 16 16"
+									>
+										<path
+											class="cls-1"
+											d="M15.84,13.85,9.25.78A1.38,1.38,0,0,0,7.4.14a1.35,1.35,0,0,0-.65.64L.16,13.85a1.53,1.53,0,0,0,0,1.44A1.41,1.41,0,0,0,1.41,16H14.59a1.41,1.41,0,0,0,1.21-.71A1.53,1.53,0,0,0,15.84,13.85ZM7,4.83A.93.93,0,0,1,8,4a.93.93,0,0,1,1,.83V8.17A.93.93,0,0,1,8,9a.93.93,0,0,1-1-.83ZM8,14a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,8,14Z"
+										/>
+									</svg>
+									<div class="ml-1">Pending</div>
+									<button class="border bg-content px-2 -py-1 ml-auto text-xs">VALIDATE</button>
+								</div>
+								<div class="bg-warning-lightest p-3 text-xs mt-2">
+									<div class="font-semibold mb-1"> The agent has not initated communication</div>
+									<ul class="list-disc ml-4">
+										<li>Make sure agent is installed</li>
+										<!-- <li
+											>Once the agent is installed please click on the "VALIDATE" button
+											again.</li
+										> -->
+										<li>Check agent connectivity</li>
+									</ul>
+									<!-- The agent has not initated communication. Once the agent is installed please click
+									on the "VALIDATE" button again. If the agent has already been installed please check
+									the agent connectivity. See <a href="text-info">sk165514</a> -->
+								</div>
+							</div>
+							<!--  -->
+						</div>
+					</div>
+					<div class="flex-0 border-t py-2 px-4 flex bg-aside">
+						<div class="ml-auto space-x-2">
+							<button
+								class="
+									bg-canvas
+									border border-weaker
+									text-weakest
+									justify-center
+									hover:bg-content
+									hover:border
+									active:bg-content-dark
+									active:border-weakest
+									focus:outline-none
+									px-3
+									py-1
+									min-w-[10ch]
+								"
+								>Back</button
+							>
+							<button
+								class="
+									theme theme-dark
+									justify-center
+									bg-primary
+									hover:bg-hover
+									hover:border-stronger
+									active:bg-primary-dark
+									active:border-weakest
+									border
+									focus:outline-none
+									px-3
+									py-1
+									min-w-[10ch]
+								"
+								>FINISH</button
+							>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/mockups/kubernetes/_2.vue
+++ src/mockups/kubernetes/_2.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="flex flex-1 items-center justify-center">
-		<div class=" theme theme-light flex flex-col relative rounded-md overflow-hidden shadow-xl">
-			<div class="flex-0 border-b px-4 py-2 theme theme-dark-primary font-light uppercase">Title</div>
+		<div class="theme theme-light flex flex-col relative overflow-hidden shadow-xl shadow-strong">
+			<div class="flex-0 border-b px-4 py-2 theme theme-dark-primary font-light uppercase"
+				>KUBERNETES ONBOARDING</div
+			>
 			<div class="flex flex-1 flex-row">
 				<div class="flex-0 flex flex-col border-r theme theme-dark-primary p-6 text-xs">
 					<div class="flex items-center">
@@ -69,7 +71,7 @@
 					<div class="h-10 w-0 ml-[11px] my-1 border-r-bold border-strongest"></div>
 					<div class="flex items-center">
 						<div class="flex items-center justify-center h-6 w-6 bg-[#ffffff] rounded-full">
-							<div class="text-md font-semibold" style="color: var(--color-primary)">3</div>
+							<div class="text-md font-semibold" style="color: var(--color-primary)">4</div>
 						</div>
 						<div class="ml-2 uppercase">Summary</div>
 					</div>
@@ -77,32 +79,63 @@
 				<div class="flex flex-1 flex-col max-w-3xl">
 					<div class="flex-1 p-6 bg-canvas">
 						<div class="text-2xl">Onboarding Summary</div>
-						<div class="grid grid-cols-6 gap-4 my-8">
+						<div class="bg-info-lightest p-2 text-xs flex items-center mt-3">
+							<div class="flex items-center justify-center">
+								<svg width="16" height="16" viewBox="0 0 16 16">
+									<path
+										d="M8,0a8,8,0,1,0,8,8A8,8,0,0,0,8,0ZM8,13H8a1,1,0,0,1-1-1V7A1,1,0,0,1,8,6H8A1,1,0,0,1,9,7v5A1,1,0,0,1,8,13ZM9,4A1,1,0,1,1,8,3,1,1,0,0,1,9,4Z"
+									/>
+								</svg>
+							</div>
+							<div class="flex-1 ml-2">
+								To validate the onboarding and test the connectivity, install the agent on the cluster.
+							</div>
+						</div>
+						<div class="grid grid-cols-5 gap-6 my-8">
 							<div class="font-semibold">Claster:</div>
-							<div class="col-span-5">My_Cluster</div>
-							<!--  -->
+							<div class="col-span-4">My_Cluster</div>
+
+							<div class="font-semibold">Deployment:</div>
+							<div class="col-span-4">
+								<div class="">
+									<div class="flex">
+										<div class="bg-content border flex-1 self-center">
+											<div class="h-1 w-4/6 bg-danger"></div>
+										</div>
+										<div class="flex justify-end ml-2"> 42 / 63 </div>
+									</div>
+								</div>
+							</div>
+
 							<div class="font-semibold">Status:</div>
-							<div class="col-span-5">Successfuly created</div>
+
+							<div class="col-span-4">
+								<div class="flex flex-1">
+									<svg
+										version="1.1"
+										id="Layer_1"
+										x="0px"
+										y="0px"
+										height="16"
+										width="16"
+										viewBox="0 0 16 16"
+										style="enable-background: new 0 0 16 16"
+										xml:space="preserve"
+									>
+										<path fill="red" d="M8,0C3.6,0,0,3.6,0,8s3.6,8,8,8s8-3.6,8-8S12.4,0,8,0z" />
+										<path
+										fill="white"
+											d="M11.7,10.3c0.4,0.4,0.4,1,0,1.4c0,0,0,0,0,0c-0.4,0.4-1,0.4-1.4,0c0,0,0,0,0,0L8,9.4l-2.3,2.3 	c-0.4,0.4-1,0.4-1.4,0c0,0,0,0,0,0c-0.4-0.4-0.4-1,0-1.4c0,0,0,0,0,0L6.6,8L4.3,5.7c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0L8,6.6l2.3-2.3 	c0.4-0.4,1-0.4,1.4,0s0.4,1,0,1.4L9.4,8L11.7,10.3z"
+										/>
+									</svg>
+									<div class="ml-1">Error</div>
+								</div>
+								<div class="bg-danger-lightest p-3 mt-2">
+
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio possimus aliquid nobis vero earum quas incidunt quisquam ab quod enim ipsa minima adipisci, odit eveniet officia, fugiat, tempora ipsam distinctio. <a class="text-info" href="text-info">See sk165514</a>
+								</div>
+							</div>
 							<!--  -->
-							<div class="font-semibold">Communication:</div>
-							<div class="col-span-5">Waiting</div>
-							<!--  -->
-						</div>
-
-						<div class=""
-							>To validate the onboarding and test the connectivity, install the agent on the cluster and
-							click on
-						</div>
-						<button class="border bg-content px-2 py-1">Validate</button>
-
-						<div class="bg-aside border my-8">
-							<div class="h-1 w-4/6 bg-info-darkest"></div>
-						</div>
-
-						<div class="">
-							The agent has not initated communication. Once the agent is installed please click on the
-							"VALIDATE" button again. If the agent has already been installed please check the agent
-							connectivity. See <a href="text-info">sk165514</a>
 						</div>
 					</div>
 					<div class="flex-0 border-t py-2 px-4 flex bg-aside">
